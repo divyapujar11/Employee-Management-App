36a64a45483e4e8d88ec930c7d377266d48a34bc
C Pujar <divya.c-pujar@capgemini.com>
2023-01-31 12:38:49 +0530

    created employee management microservice

---
+++ Backend-SB/emp-mng/src/test/java/com/employee/empmng/EmpMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.empmng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Employee-Management-App
+react:
+1) Router
+2) Components
+3) Service - axios,http,library
+
+sb:
+1) controller
+2) service
+3) repository
